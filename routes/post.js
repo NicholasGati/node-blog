986c8c65292ab7461476cfcdeefb4f8b18e8bd8c
NicholasGati <ngati016@hotmail.com>
2017-02-01 15:38:55 -0500

    added some more to routes/post

--- routes/post.js
+++ routes/post.js
@@ -10,9 +10,36 @@ router.get("/all", (req, res, next) => {
     posts.forEach((post) => {
       postMap[user.id] = { email: user.email, _id: post.id, body: post.body, title: post.title };
     });
-    res.send(postMap);
+    res.render("post/index", { posts: postMap });
   });
 });
 
+router.get("/:id", isLoggedIn, (req, res, next) => {
+  // TODO get post id and show details of that post
+  res.render("post/show");
+});
+
+router.get("/new", isLoggedIn, (req, res, next) => {
+  // TODO create form for new post
+  res.render("post/new");
+});
+
+router.post("/create", isLoggedIn, (req, res, next) => {
+  // TODO actions for  new post creation
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+function isNotLoggedIn(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 module.exports = router;
